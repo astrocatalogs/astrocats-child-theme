f6a14941797989e63c4e4f46e7cd3056a78d3e2f
guillochon <guillochon@gmail.com>
2016-06-02 08:41:08 -0700

    Critical fix for Safari as it cannot accept default parameters for JS function arguments.

--- resize-iframe.js
+++ resize-iframe.js
@@ -1,4 +1,5 @@
-function resizeIframe(obj, height = 950) {
+function resizeIframe(obj, height) {
+	if (typeof(height) === 'undefined') height = 950;
 	obj.style.height = obj.contentWindow.document.body.scrollHeight + height + 'px';
 	document.getElementById('loading').style.display = 'none';
 }
