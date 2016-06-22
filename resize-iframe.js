16ee9a2a6eab885654104983491461ad13cfa8a8
guillochon <guillochon@gmail.com>
2016-06-22 05:18:37 -0700

    Resize every 1.5 seconds in case window resized.

--- resize-iframe.js
+++ resize-iframe.js
@@ -1,7 +1,8 @@
 function resizeIframe(obj) {
 	var iframeWin = obj.contentWindow || obj.contentDocument.parentWindow;
 	if (iframeWin.document.body) {
-		setTimeout( function() { obj.height = (iframeWin.document.body.scrollHeight || iframeWin.document.documentElement.scrollHeight) + 30 + 'px'; }, 10);
+		setTimeout( function() { obj.height = parseInt(iframeWin.document.documentElement.scrollHeight) + 'px'; }, 10);
+		setInterval( function() { obj.height = parseInt(iframeWin.document.documentElement.scrollHeight) + 'px'; }, 1500);
 	}
 	document.getElementById('loading').style.display = 'none';
 }
