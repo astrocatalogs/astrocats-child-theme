a7f5da2b7c201f6041a780e39e3493acac66ef2f
guillochon <guillochon@gmail.com>
2016-10-14 18:35:20 -0700

    Making usable by multiple catalogs

--- event.php
+++ event.php
@@ -12,6 +12,10 @@
  * @since parabola 0.5
  */
 
+$tt = explode("\n", file_get_contents(__DIR__ . '/tt.dat'));
+$stem = trim($tt[0]);
+$modu = trim($tt[1]);
+
 global $wp_query;
 get_header();
 ?>
@@ -24,15 +28,14 @@ get_header();
 	<div class="page type-page status-publish hentry">
 <?php
 	$rootpath = '/root/';
-	$htmlpath = 'astrocats/astrocats/tidaldisruptions/output/html/';
-	//$htmlpath = 'astrocats/astrocats/tidaldisruptions/output/html/';
+	$htmlpath = 'astrocats/astrocats/' + $modu + '/output/html/';
 	function loadEventFrame($name) {
 		global $rootpath, $htmlpath;
 		if (file_exists($rootpath.$htmlpath.rawurldecode($name).'.html') ||
 			file_exists($rootpath.$htmlpath.rawurldecode($name).'.html.gz')) { ?>
-			<div id="loading" style="text-align:center;"><img src="https://tde.space/wp-content/themes/astrocats-child-theme/loading.gif"><br>Loading...</div>
+			<div id="loading" style="text-align:center;"><img src="https://<?php echo $stem; ?>.space/wp-content/themes/astrocats-child-theme/loading.gif"><br>Loading...</div>
 			<div style="overflow:auto;-webkit-overflow-scrolling:touch">
-			<iframe width=100% scrolling="no" src="https://tde.space/<?php echo $htmlpath.$name; ?>.html" style="display:block;border:none;width=100%;" onload="resizeIframe(this)"></iframe>
+			<iframe width=100% scrolling="no" src="https://<?php echo $stem; ?>.space/<?php echo $htmlpath.$name; ?>.html" style="display:block;border:none;width=100%;" onload="resizeIframe(this)"></iframe>
 			</div>
 <?php 		return true;
 		}
@@ -47,12 +50,12 @@ get_header();
 			(substr($eventname, 0, 2) == 'SN' && is_numeric(substr($eventname, 2, 3)) && strlen($eventname) == 6)) {
 			$eventname = strtoupper($eventname);
 		}
-		$str = file_get_contents('/var/www/html/tde/astrocats/astrocats/tidaldisruptions/output/names.min.json');
+		$str = file_get_contents('/var/www/html/' + $stem + '/astrocats/astrocats/' + $modu + '/output/names.min.json');
 		$json = json_decode($str, true);
 		$found = false;
 		foreach ($json as $name => $entry) {
 			foreach ($entry as $alias) {
-				if(strpos($alias, $eventname) !== false) {
+				if($alias == $eventname) {
 					if (loadEventFrame($name)) {
 						$found = true;
 					} else {
